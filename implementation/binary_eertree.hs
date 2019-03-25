67b4a344942b201e223a476d72fcb4412349b9d8
khazhix <xwayz@ya.ru>
2019-03-25 18:54:34 +0300

    added merge

--- implementation/binary_eertree.hs
+++ implementation/binary_eertree.hs
@@ -4,7 +4,8 @@
 
 module Binary where
 
-import           Data.List (transpose, nub)
+import           Data.List (transpose, nub, maximumBy)
+import           Data.Ord  (comparing)
 
 -- | A node of an infinite unary eertree.
 data Node a = Node
@@ -181,17 +182,103 @@ add c t = g $
               | pref t == "" = t{suff = "", maxPrefLink = maxSuffLink t}
               | otherwise = t 
 
+prepend :: Char -> Eertree Char -> Eertree Char
+prepend c t = g $
+    case suff t of
+        _ | len (maxPrefLink t) == -1 -> t
+            { maxPrefLink = nodeByValue [c]
+            , pref        = pref t ++ [c]
+            , suff        = suff t
+            , pals        = pals t ++ [nodeByValue [c]]
+            } 
+        c':cs | c == c' -> t
+            { maxPrefLink = newPrefSuff    
+            , pref        = pref t ++ [c]
+            , suff        = cs
+            , pals        = pals t ++ [newPrefSuff]
+            }
+        _ -> prepend c t'
+    where
+            newPrefSuff = nodeByValue (c: value (maxPrefLink t) ++ [c])
+            t' = t
+                { maxPrefLink = link (maxPrefLink t)
+                , suff        = drop (len (link (maxPrefLink t))) (value (maxPrefLink t)) ++ suff t 
+                }
+            g t
+              | suff t == "" = t{pref = "", maxSuffLink = maxPrefLink t}
+              | otherwise = t 
+
+
 gen :: Int -> [String]
 gen 0 = [[]]
 gen n = concat
         [ ['a':s, 'b':s]
         | s <- gen (n - 1) ]
 
+checkPrep :: Int -> Bool
+checkPrep n = all (\x -> suff (eertree x) == suff (eertree' x)) (gen n)
+
 eertree :: String -> Eertree Char
 eertree = foldl (flip add) empty
 
+eertree' :: String -> Eertree Char
+eertree' s = foldl (flip prepend) empty (reverse s)
+
 palindromesOf :: String -> [String]
 palindromesOf = nub . map value . pals . eertree
+
+mergeEertees :: Eertree Char -> Eertree Char -> Eertree Char
+mergeEertees t1 t2 = Eertree 
+            { maxPrefLink = newPrefLink            
+            , maxSuffLink = newSuffLink 
+            , pref        = reverse (take ((length (fstStr ++ sndStr)) - len newSuffLink) (fstStr ++ sndStr))
+            , suff        = drop (len newPrefLink) (fstStr ++ sndStr)
+            , pals        = newPals ++ pals t1 ++ pals t2
+            }
+    where
+        newPrefLink  = nodeByValue (maximumBy (comparing length) wholePref) -- ^ taking best from candidates
+        newSuffLink  = nodeByValue (maximumBy (comparing length) wholeSuff) -- ^ taking best from candidates
+        wholePref    = map (\(_,x,_) -> x) (filter (\(x,_,_) -> x == "") foundPals) ++ [value (maxPrefLink t1)] -- ^ candidates for being maxPref
+        wholeSuff    = map (\(_,x,_) -> x) (filter (\(_,_,x) -> x == "") foundPals) ++ [value (maxSuffLink t2)] -- ^ candidates for being maxSuff
+        foundPals    = rightPals ++ leftPals ++ middlePals             -- ^ all information about new found palinfromes
+        newPals      = map nodeByValue (map (\(_,x,_) -> x) foundPals) -- ^ adding to pals list (which is list of Nodes)
+        middlePals   = (palsFromMiddle (reverse fstStr) sndStr "")     -- ^ searching for palinfromes from (joint) middle evenly
+        leftPals     = (concat (zipWith3 palsFromMiddle fstLeftPals sndLeftPals midLeftPals)) 
+                -- ^ searching for palinfromes near suffix of first str
+        sndLeftPals  = replicate (length midLeftPals) sndStr
+        fstLeftPals  = map (\x -> reverse(take ((length fstStr) - (length x)) fstStr)) midLeftPals
+        midLeftPals  = map value (collectSuffixes (maxSuffLink t1))
+                -- ^ creating lists of different inputs depending on palinfrome center
+        rightPals    = (concat (zipWith3 palsFromMiddle fstRightPals sndRightPals midRightPals))
+                -- ^ searching for palinfromes near prefix of second str
+        fstRightPals = replicate (length midRightPals) (reverse fstStr)
+        sndRightPals = map (\x -> drop (length x) sndStr) midRightPals
+        midRightPals = map value (collectSuffixes (maxPrefLink t2))
+                -- ^ creating lists of different inputs depending on palinfrome center
+        fstStr       = reverse (pref t1) ++ value (maxSuffLink t1) -- ^ string of first tree
+        sndStr       = reverse (pref t2) ++ value (maxSuffLink t2) -- ^ string of second tree
+
+-- | Creates palindrome with the middle of value i
+-- | Contain information about prefix before palinfrome and suffix after
+-- >>> palsFromMiddle "aba" "aba" ""
+-- [("ba","aa","ba"),("a","baab","a"),("","abaaba","")]
+palsFromMiddle :: String -> String -> String -> [(String, String, String)]
+palsFromMiddle _ [] _ = []
+palsFromMiddle [] _ _ = []
+palsFromMiddle (c:s) (c':s') i
+    | c == c'   = (s,newOne,s'):palsFromMiddle s s' newOne
+    | otherwise = []
+    where
+        newOne = c:i ++ [c]
+
+collectSuffixes :: Node Char -> [Node Char]
+collectSuffixes a
+    | len a == 0 = []
+    | otherwise  = a: collectSuffixes (link a)
+
+
+checkMerge :: String -> String -> [String]
+checkMerge s s' = map value (pals (mergeEertees (eertree s) (eertree s')))
 
 a216264 :: Int -> Int
 a216264 n = length strings
